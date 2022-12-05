c6ee5122a8b44d6f27c9607e9a9792d27379273a
JhonnyO3 <jhonatas2004@gmail.com>
2022-12-05 20:36:51 -0300

    Class pojo com validacoes

---
+++ Models/Filmes.cs
@@ -0,0 +1,22 @@
+namespace FilmesApi.Models
+{
+
+    using System.ComponentModel.DataAnnotations;
+
+
+    public class Filmes
+    {
+
+        [Required(ErrorMessage = "O titulo é obrigatorio!")]
+        [MaxLength(100, ErrorMessage = "Tamanho maximo 100")]
+        public string? Titulo { get; set; }
+        [MaxLength(100, ErrorMessage = "Tamanho maximo 100")]
+        [Required(ErrorMessage = "O Genero é obrigatorio!")]
+        public string? Genero { get; set; }
+        [Range(10, 300, ErrorMessage = "Tamanho entre 10 e 300!")]
+        [Required(ErrorMessage = "A duracao é obrigatorio!")]
+        public int Duracao { get; set; }
+
+
+    }
+}
