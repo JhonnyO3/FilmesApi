53fd7ab951f7e54611f9298018bd5df05850d59c
JhonnyO3 <jhonatas2004@gmail.com>
2022-12-05 21:34:43 -0300

    melhorias no POST

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -4,28 +4,41 @@ namespace FilmesApi.Controllers
     using FilmesApi.Models;
     [ApiController] //Indica que a classe representa uma controller
     [Route("[controller]")] //Indica que as requisicoes serao direcionadas para a class FilmeController
-    public class FilmeController : ControllerBase {
+    public class FilmeController : ControllerBase
+    {
 
         private static List<Filmes> filmes = new List<Filmes>();
 
         private static int id;
 
         [HttpPost]
-        public void adicionaFilme([FromBody]Filmes filme) {
+        public IActionResult adicionaFilme([FromBody] Filmes filme)
+        {
             filme.Id = id++;
             filmes.Add(filme);
             Console.WriteLine($"{filme.Titulo} Foi Registrado em um request POST.");
+            return CreatedAtAction(nameof(buscaFilmes), new {id = filme.Id}, filme);
 
         }
 
-        [HttpGet] 
-        public IEnumerable<Filmes> exibeFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50) {
+        [HttpGet]
+        public IEnumerable<Filmes> exibeFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
+        {
+
             return filmes.Skip(skip).Take(take);
         }
 
         [HttpGet("{id}")]
-        public Filmes? buscaFilmes(int id) {
-            return filmes.FirstOrDefault(filme => filme.Id == id); //Funcao vai iterar buscando o filme que possui o id igual ao passado na resquisicao, caso nao encontre, o valor sera Null/Default
+        public IActionResult buscaFilmes(int id)
+        {
+
+            var filme = filmes.FirstOrDefault(filme => filme.Id == id); //Funcao vai iterar buscando o filme que possui o id igual ao passado na resquisicao, caso nao encontre, o valor sera Null/Default
+
+            if (filme == null)
+            {
+                return NotFound();
+            }
+            return Ok();
         }
 
     }
